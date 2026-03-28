a07f187c74b16596288a35755eb2df78c411ec10
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2026-03-28 14:27:57 +0300

    feat: road system implementation

---
+++ Assets/_Game/Scripts/Road/RoadChunk.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RoadChunk : MonoBehaviour
+{
+    [SerializeField] private float chunkLength = 20f;
+    
+    public float ChunkLength => chunkLength;
+
+
+    public void ResetChunk()
+    {
+        
+    }
+}
---
+++ Assets/_Game/Scripts/Road/RoadSpawner.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    [SerializeField] private RoadChunk chunkPrefab;
+    [SerializeField] private int visibleChunkCount = 5;
+    [SerializeField] private Transform playerTransform;
+
+    private ObjectPool<RoadChunk> _chunkPool;
+    private readonly List<RoadChunk> _activeChunks = new();
+    private float _nextSpawnZ = 0f;
+
+
+    private void Awake()
+    {
+        _chunkPool = new ObjectPool<RoadChunk>(
+            createFunc: () => Instantiate(chunkPrefab),
+            actionOnGet: chunk => chunk.gameObject.SetActive(true),
+            actionOnRelease: chunk => chunk.gameObject.SetActive(false),
+            actionOnDestroy: chunk => Destroy(chunk.gameObject),
+            defaultCapacity: visibleChunkCount + 2
+        );
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < visibleChunkCount; i++)
+        {
+            SpawnChunk();
+        }
+    }
+
+    private void Update()
+    {
+        RecycleChunks();
+        FillAhead();
+    }
+
+    private void SpawnChunk()
+    {
+        RoadChunk chunk = _chunkPool.Get();
+        chunk.transform.position = new Vector3(0, 0, _nextSpawnZ);
+        chunk.ResetChunk();
+        _activeChunks.Add(chunk);
+        _nextSpawnZ += chunk.ChunkLength;
+    }
+
+    private void RecycleChunks()
+    {
+        if(_activeChunks.Count == 0) return;
+        
+        RoadChunk oldest = _activeChunks[0];
+        float recyclePoint = playerTransform.position.z - oldest.ChunkLength;
+
+        if (oldest.transform.position.z < recyclePoint)
+        {
+            _activeChunks.RemoveAt(0);
+            _chunkPool.Release(oldest);
+            SpawnChunk();
+        }
+    }
+
+    private void FillAhead()
+    {
+        float aheadDistance = playerTransform.transform.position.z + (visibleChunkCount * chunkPrefab.ChunkLength);
+
+        while (_nextSpawnZ < aheadDistance)
+        {
+            SpawnChunk();
+        }
+    }
+}
